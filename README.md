b77153cb0d3cf0a11cc576b7ab4fc5f29e8bffd0
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-06-15 23:00:08 +1000

    tweaks

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# review-bootcamp
+# Review Bootcamp
+
+Interview Must-Reads
+- [Developer Survey](https://insights.stackoverflow.com/survey/2018/?utm_source=Iterable&utm_medium=email&utm_campaign=dev-survey-2018-promotion)
+- [Writing a Resume](http://steve-yegge.blogspot.com/2007_09_01_archive.html)
+- [Preparing for an interview](http://steve-yegge.blogspot.com/2008/03/get-that-job-at-google.html)
--- algorithm/src/main/algorithm/MergeSortAlgo.java
+++ algorithm/src/main/algorithm/MergeSortAlgo.java
@@ -6,59 +6,59 @@ import java.util.Arrays;
  * Keeps breaking the array into two parts until base, then merges them in sorted order
  */
 public class MergeSortAlgo {
-    public static void main(String[] args){
-        int[] test = {12,7,5,0,9,2};
-        MergeSortAlgo al = new MergeSortAlgo();
-        al.sort(test, 0, test.length -1);
-        System.out.println("DONE");
+    public static void main(String[] args) {
+        int[] test = {12, 7, 5, 0, 9, 2};
+        sort(test, 0, test.length - 1);
         Arrays.stream(test).forEach(System.out::println);
 
 
     }
 
-    public void sort(int[] A, int start, int end){
-        if(start < end){
-            int mid = (start + end) /2;
-            sort(A, start, mid);
-            sort(A, mid +1, end);
-            merge(A, start, mid, end);
+    public static void sort(int[] unsorted, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            sort(unsorted, start, mid);
+            sort(unsorted, mid + 1, end);
+            merge(unsorted, start, mid, end);
         }
     }
 
-    public void merge(int[] A, int start, int mid, int end) {
-      int [] temp = new int[end - start + 1];
-      int leftSlot = start;
-      int rightSlot = mid + 1;
-      int k = 0;
+    public static void merge(int[] unsorted, int start, int mid, int end) {
+        // the size of the temp array is critical here.
+        int[] temp = new int[end - start + 1];
+        int leftSlot = start;
+        int rightSlot = mid + 1;
+        int k = 0;
 
-      while(leftSlot <= mid && rightSlot <= end){
-          if(A[leftSlot] < A[rightSlot]){
-              temp[k] = A[leftSlot];
-              leftSlot++;
-          } else {
-            temp[k] = A[rightSlot];
-            rightSlot++;
-          }
-          k++;
-      }
+        while (leftSlot <= mid && rightSlot <= end) {
+            if (unsorted[leftSlot] < unsorted[rightSlot]) {
+                temp[k] = unsorted[leftSlot];
+                leftSlot++;
+            } else {
+                temp[k] = unsorted[rightSlot];
+                rightSlot++;
+            }
+            k++;
+        }
 
-      //right side sorted
-      if(leftSlot <= mid){
-            while(leftSlot <=mid){
-                temp[k] = A[leftSlot];
+        //right side sorted or left side already sorted
+        if (leftSlot <= mid) {
+            while (leftSlot <= mid) {
+                temp[k] = unsorted[leftSlot];
                 leftSlot++;
                 k++;
             }
-      } else if(rightSlot >= end){
-          while(rightSlot <= end){
-              temp[k] = A[rightSlot];
-              rightSlot++;
-              k++;
-          }
-      }
+        } else if (rightSlot <= end) {
+            while (rightSlot <= end) {
+                temp[k] = unsorted[rightSlot];
+                rightSlot++;
+                k++;
+            }
+        }
 
-      for(int i =0; i< temp.length; i++){
-          A[start+i] = temp[i];
-      }
+        //we are replacing on the some spots on the unsorted array based on the size of the temp one
+        for (int i = 0; i < temp.length; i++) {
+            unsorted[start + i] = temp[i];
+        }
     }
 }
