6a0a964e58b5c57e2e6838cfeaf2dc5102235f68
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-08-09 13:04:22 +1000

    Robot tweak

--- algorithm/src/main/brainteasers/Robot.java
+++ algorithm/src/main/brainteasers/Robot.java
@@ -1,13 +1,21 @@
 package brainteasers;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Robot {
-    private final static String PICKUP = "P", MOVE = "M", LOWER = "L";
     private static int[] stash = new int[10];
+    private static HashMap<Integer, String> map = new HashMap<Integer, String>();
 
     public static void main(String[] args) {
-        String command = "PMLPMMMLPMLPMML";
+        map.put(10, "A");
+        map.put(11, "B");
+        map.put(12, "C");
+        map.put(13, "D");
+        map.put(14, "E");
+        map.put(15, "F");
+
+        String command = "PMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMLPMMMLPMLPMMLPMMMMMMMMMMLPMMMMMMMMMML";
         executeCommand(command);
     }
 
@@ -15,21 +23,35 @@ public class Robot {
         int block = 0;
         int stashLocation = 0;
         for (Character c : command.toCharArray()) {
-            if (String.valueOf(c).equals(PICKUP)) {
-                block = 1;
-            } else if (String.valueOf(c).equals(MOVE)) {
-                if (stashLocation < 10) {
-                    stashLocation++;
-                }
-            } else if (String.valueOf(c).equals(LOWER)) {
-                if (block != 0) {
-                    stash[stashLocation] = stash[stashLocation] + 1;
-                    block = 0;
-                    stashLocation = 0;
-                }
+            switch (c) {
+                case 'P':
+                    if (block == 0) {
+                        block = 1;
+                    } else {
+                        stashLocation = 0;
+                    }
+                    break;
+                case 'M':
+                    if (stashLocation < 9) {
+                        stashLocation++;
+                    }
+                    break;
+                case 'L':
+                    if (block != 0 && stash[stashLocation] < 15) {
+                        stash[stashLocation] = stash[stashLocation] + 1;
+                        block = 0;
+                        stashLocation = 0;
+                    }
+                    break;
             }
         }
-        Arrays.stream(stash).forEach(System.out::print);
+        Arrays.stream(stash).forEach(val -> {
+            if (map.containsKey(val)) {
+                System.out.print(map.get(val));
+            } else {
+                System.out.print(val);
+            }
+        });
     }
 }
 
