7c5af8324821acc7de2ce8e307e0d3e05936a92b
mel3kings <meltatlonghari3@gmail.com>
2018-10-27 16:26:13 +1100

    Working POC: Amazon MQ topics

---
+++ algorithm/Algorithm/src/main/java/Snippets/ActiveMQController.java
@@ -0,0 +1,81 @@
+package Snippets;
+
+import static Snippets.Constants.ACKNOWLEDGE_MODE;
+import static Snippets.Constants.DELIVERY_MODE;
+import static Snippets.Constants.ENDPOINT;
+import static Snippets.Constants.PASSWORD;
+import static Snippets.Constants.QUEUE;
+import static Snippets.Constants.USERNAME;
+
+import java.util.stream.IntStream;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.jms.pool.PooledConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+public class ActiveMQController {
+
+
+  public static void main(String[] args) throws JMSException {
+    // Create a connection factory.
+    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ENDPOINT);
+
+    // Specify the username and password.
+    connectionFactory.setUserName(USERNAME);
+    connectionFactory.setPassword(PASSWORD);
+
+    // Create a pooled connection factory.
+    PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
+    pooledConnectionFactory.setConnectionFactory(connectionFactory);
+    pooledConnectionFactory.setMaxConnections(10);
+
+    // Create a JmsTemplate for the producer.
+    JmsTemplate producerJmsTemplate = new JmsTemplate();
+    producerJmsTemplate.setConnectionFactory(pooledConnectionFactory);
+    producerJmsTemplate.setDefaultDestination(new ActiveMQQueue(QUEUE));
+    producerJmsTemplate.setSessionAcknowledgeMode(ACKNOWLEDGE_MODE);
+    producerJmsTemplate.setDeliveryMode(DELIVERY_MODE);
+
+    // Create a message creator.
+    MessageCreator messageCreator = new MessageCreator() {
+      public Message createMessage(Session session) throws JMSException {
+        return session.createTextMessage("Hello from Spring Amazon MQ!");
+      }
+    };
+
+    // Send the message.
+    IntStream.range(0, 10).forEach(i -> {
+      producerJmsTemplate.send(messageCreator);
+      System.out.println("Message sent." + i);
+    });
+
+    // Clean up the producer.
+    // producer JmsTemplate will close underlying sessions and connections.
+
+    // Create a JmsTemplate for the consumer.
+    // Note: Consumers should not use PooledConnectionFactory.
+    JmsTemplate consumerJmsTemplate = new JmsTemplate();
+    consumerJmsTemplate.setConnectionFactory(connectionFactory);
+    consumerJmsTemplate.setDefaultDestination(new ActiveMQQueue(QUEUE));
+    consumerJmsTemplate.setSessionAcknowledgeMode(ACKNOWLEDGE_MODE);
+    consumerJmsTemplate.setReceiveTimeout(1000);
+
+    // Begin to wait for messages.
+    try {
+      while (true) {
+        Message consumerMessage = consumerJmsTemplate.receive();
+        // Receive the message when it arrives.
+        TextMessage consumerTextMessage = (TextMessage) consumerMessage;
+        System.out.println("Message received: " + consumerTextMessage.getText());
+
+      }
+    } finally {
+      pooledConnectionFactory.stop();
+
+    }
+  }
+}
---
+++ algorithm/Algorithm/src/main/java/Snippets/ActiveMQRouterController.java
@@ -0,0 +1,81 @@
+package Snippets;
+
+
+import static Snippets.Constants.ACKNOWLEDGE_MODE;
+import static Snippets.Constants.CONSUMER1_TOPIC;
+import static Snippets.Constants.DELIVERY_MODE;
+import static Snippets.Constants.ENDPOINT;
+import static Snippets.Constants.PASSWORD;
+import static Snippets.Constants.PRODUCER_TOPIC;
+import static Snippets.Constants.USERNAME;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.activemq.jms.pool.PooledConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+public class ActiveMQRouterController {
+
+  public static void main(String[] args) throws JMSException {
+    // Create a connection factory.
+    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ENDPOINT);
+
+    // Specify the username and password.
+    connectionFactory.setUserName(USERNAME);
+    connectionFactory.setPassword(PASSWORD);
+
+    // Create a pooled connection factory.
+    PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
+    pooledConnectionFactory.setConnectionFactory(connectionFactory);
+    pooledConnectionFactory.setMaxConnections(10);
+
+    // Create a JmsTemplate for the producer.
+    JmsTemplate producerJmsTemplate = new JmsTemplate();
+    producerJmsTemplate.setConnectionFactory(pooledConnectionFactory);
+    producerJmsTemplate.setDefaultDestination(new ActiveMQTopic(PRODUCER_TOPIC));
+    producerJmsTemplate.setSessionAcknowledgeMode(ACKNOWLEDGE_MODE);
+    producerJmsTemplate.setDeliveryMode(DELIVERY_MODE);
+
+    // Create a message creator.
+    MessageCreator messageCreator = new MessageCreator() {
+      public Message createMessage(Session session) throws JMSException {
+        return session.createTextMessage("ROUTER");
+      }
+    };
+
+    // Send the message.
+    producerJmsTemplate.send(messageCreator);
+    System.out.println("Message sent.");
+
+    // Clean up the producer.
+    // producer JmsTemplate will close underlying sessions and connections.
+
+    // Create a JmsTemplate for the consumer.
+    // Note: Consumers should not use PooledConnectionFactory.
+
+    JmsTemplate consumerJmsTemplate = new JmsTemplate();
+    consumerJmsTemplate.setConnectionFactory(connectionFactory);
+    consumerJmsTemplate.setDefaultDestination(new ActiveMQQueue(CONSUMER1_TOPIC));
+    consumerJmsTemplate.setSessionAcknowledgeMode(ACKNOWLEDGE_MODE);
+    consumerJmsTemplate.setReceiveTimeout(1000);
+
+    // Begin to wait for messages.
+    Message consumerMessage = consumerJmsTemplate.receive();
+
+    // Receive the message when it arrives.
+    TextMessage consumerTextMessage = (TextMessage) consumerMessage;
+    System.out.println("Message received: " + consumerTextMessage.getText());
+
+    // Clean up the consumer.
+    // consumer JmsTemplate will close underlying sessions and connections.
+    pooledConnectionFactory.stop();
+  }
+}
+
