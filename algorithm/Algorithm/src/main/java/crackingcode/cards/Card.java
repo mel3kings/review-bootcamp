123c0ff21517c46446101a2b8499a70f778822a1
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-12-05 22:35:16 +1100

    OOD

---
+++ algorithm/Algorithm/src/main/java/crackingcode/cards/Card.java
@@ -0,0 +1,46 @@
+package crackingcode.cards;
+
+public class Card {
+
+
+    private CardSuite cardSuite;
+    private int number;
+
+    public void setCardSuite(CardSuite cardSuite) {
+        this.cardSuite = cardSuite;
+    }
+
+    public CardSuite getCardSuite() {
+        return cardSuite;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setNumber(FaceCard faceCard){
+        this.number = faceCard.getNumber();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public enum FaceCard {
+        KING(10), QUEEN(10);
+        int number;
+
+        FaceCard(int number) {
+            this.number = number;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+    }
+
+    public enum CardSuite {
+        SPADE, HEART, DIAMOND, CLUBS
+    }
+
+}
---
+++ algorithm/Algorithm/src/main/java/crackingcode/cards/SampleGame.java
@@ -0,0 +1,16 @@
+package crackingcode.cards;
+
+public class SampleGame {
+    public static void main(String[] args){
+        Card c = new Card();
+        c.setCardSuite(Card.CardSuite.CLUBS);
+        c.setNumber(10);
+
+        Card king = new Card();
+        c.setCardSuite(Card.CardSuite.DIAMOND);
+        c.setNumber(Card.FaceCard.KING);
+
+        assert(king.getNumber() == 10);
+
+    }
+}
