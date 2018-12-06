b424b2289e021678c629b533c42601fe0b5d8e0c
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-12-06 22:09:13 +1100

    strings problems

--- algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/AnagramArray.java
+++ algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/AnagramArray.java
@@ -1,4 +1,4 @@
-package algorithm;
+package crackingcode.stringsAndArrays;
 
 import java.util.Arrays;
 
---
+++ algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/SubstringChecker.java
@@ -0,0 +1,15 @@
+package crackingcode.stringsAndArrays;
+
+public class SubstringChecker {
+
+    public static void main(String[] args){
+        String wordCheck = "waterbottle";
+        assert(isSubstring(wordCheck, "erbottlewat"));
+    }
+
+    public static boolean isSubstring(String actualWord, String checkWord){
+        String concatenated = actualWord + actualWord;
+        return concatenated.contains(checkWord);
+    }
+}
+
