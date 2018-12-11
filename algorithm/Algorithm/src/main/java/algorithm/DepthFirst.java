5089d4220712d01f28eb8d8ed8e95f5a423f90c3
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-12-11 16:12:03 +1100

    second dfs

--- algorithm/Algorithm/src/main/java/algorithm/DepthFirst.java
+++ algorithm/Algorithm/src/main/java/algorithm/DepthFirst.java
@@ -11,18 +11,19 @@ public class DepthFirst {
     //TREE IS:
     //       A
     //   B        F
-    //  C       D      G
-    //H  J    E
+    //  C   D        G
+    //H  J E
     public static void main(String[] args) {
         Node node = Utils.generateRootNode();
         Stack<Node> stack = new Stack<>();
         stack.push(node);
-        Node found = DFS(stack, node, "E");
-        Optional.ofNullable(found).ifPresent(result->System.out.println("found DATA: "+ result.getData()));
+        Node found = DFS(stack, node, "G");
+        Node found2 = DepthFirstSearch(stack, "X");
+        Optional.ofNullable(found2).ifPresent(result -> System.out.println("found DATA: " + result.getData()));
     }
 
     public static Node DFS(Stack<Node> stack, Node currentNode, String findMe) {
-        System.out.println("VISITING: " + currentNode.getData());
+        System.out.println("first method: " + currentNode.getData());
         if (null != currentNode && currentNode.getData().equals(findMe)) {
             return currentNode;
         } else if (!stack.isEmpty()) {
@@ -32,9 +33,30 @@ public class DepthFirst {
             if (null != currentNode.getLeft()) {
                 stack.push(currentNode.getLeft());
             }
-          return DFS(stack, stack.pop(), findMe);
+            return DFS(stack, stack.pop(), findMe);
         } else {
             return null;
         }
     }
+
+
+    public static Node DepthFirstSearch(Stack<Node> stack, String find) {
+        Node currentNode = stack.pop();
+        System.out.println("second method: " + currentNode.getData());
+        if (null != currentNode && currentNode.getData().equals(find)) {
+            return currentNode;
+        } else if (!stack.isEmpty() || currentNode.getLeft() != null || currentNode.getRight() != null) {
+
+            if (currentNode.getRight() != null) {
+                stack.push(currentNode.getRight());
+            }
+            if (currentNode.getLeft() != null) {
+                stack.push(currentNode.getLeft());
+            }
+            return DepthFirstSearch(stack, find);
+        } else {
+            return null;
+        }
+    }
+
 }
