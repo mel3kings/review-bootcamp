219a66aa7575164b5ca2c1b690ae97efa13897ef
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2019-01-06 23:34:56 +1100

    change to jdk 8

--- algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/AnagramArray.java
+++ algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/AnagramArray.java
@@ -3,32 +3,15 @@ package crackingcode.stringsandarrays;
 import java.util.Arrays;
 
 public class AnagramArray {
+
     public static void main(String[] args) {
-        System.out.println(isAnagram("algorithm", "mhtirogla"));
-        System.out.println(isAnagramEight("algorit1hm", "mhtirogla"));
+        System.out.println(isAnagram2("algorithm", "mhtirogla"));
     }
 
-    public static boolean isAnagram(String a, String b) {
+    private static boolean isAnagram2(String a, String b) {
         int[] asci = new int[256];
-        for (char c : a.toCharArray()) {
-            asci[c]++;
-        }
-        for (char c : b.toCharArray()) {
-            asci[c]--;
-        }
-
-        for (int i = 0; i < asci.length; i++) {
-            if (asci[i] != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static boolean isAnagramEight(String a, String b){
-        int[] asci = new int[256];
-        a.chars().forEach(val ->asci[val]++);
-        b.chars().forEach(val->asci[val]--);
-        return !(Arrays.stream(asci).anyMatch(value -> value != 0));
+        a.chars().forEach(val -> asci[val]++);
+        b.chars().forEach(val -> asci[val]--);
+        return !Arrays.stream(asci).anyMatch(i -> i != 0);
     }
 }
--- algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/CheckDuplicates.java
+++ algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/CheckDuplicates.java
@@ -1,31 +1,14 @@
 package crackingcode.stringsandarrays;
 
-import java.util.LinkedHashSet;
+import java.util.stream.Collectors;
 
 public class CheckDuplicates {
 
     public static void main(String[] args) {
-        System.out.println(checkDuplicate("Melchore"));
+        System.out.println(removeDuplicates("Melchore"));
     }
 
-    private static boolean checkDuplicate(String str) {
-        boolean[] listOfChars = new boolean[266];
-        for (Character c : str.toCharArray()) {
-            if (listOfChars[c]) {
-                return true;
-            }
-            listOfChars[c] = true;
-        }
-        return false;
-    }
-
-    /**
-     * Design an algorithm and write code to remove the duplicate characters in a string without using any additional buffer.
-     * NOTE: One or two additional variables are fine. An extra copy of the array is not.
-     */
     private static String removeDuplicates(String a) {
-        LinkedHashSet<Character> set = new LinkedHashSet<>();
-        a.chars().mapToObj(e -> (char) e).forEach(set::add);
-        return set.toString();
+        return a.chars().mapToObj(e -> (char) e).collect(Collectors.toSet()).toString();
     }
 }
