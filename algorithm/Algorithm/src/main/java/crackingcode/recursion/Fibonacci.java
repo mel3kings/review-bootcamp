f4f753dc0241bc603cbee0757c6fe4870684d0b6
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2019-01-13 14:39:16 +1100

    fibo stream

--- algorithm/Algorithm/src/main/java/crackingcode/recursion/Fibonacci.java
+++ algorithm/Algorithm/src/main/java/crackingcode/recursion/Fibonacci.java
@@ -1,5 +1,7 @@
 package crackingcode.recursion;
 
+import java.util.stream.Stream;
+
 /**
  * Recursive + Iterative solution
  */
@@ -9,6 +11,7 @@ public class Fibonacci {
     public static void main(String[] args) {
         System.out.println(fibonacci(8));
         System.out.println(fiboIteration(8));
+        fiboStream(20);
     }
 
     public static int fibonacci(int n) {
@@ -21,6 +24,11 @@ public class Fibonacci {
         }
     }
 
+    private static void fiboStream(int n) {
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(n).forEach(t -> System.out.print("(" + " " + t[0] + " ," + t[1] + ")"));
+    }
+
     public static int fiboIteration(int n) {
         int fibo = 0, first = 0, second = 1;
         for (int i = 1; i < n; i++) {
@@ -31,14 +39,14 @@ public class Fibonacci {
         return fibo;
     }
 
-    private static long evenFiboHacker(long number){
-        long holder = 1,totalSum = 0, i =1, result = 0;
-        for(; i<number;  holder = i, i = totalSum ){
-            if(i+holder > number){
+    private static long evenFiboHacker(long number) {
+        long holder = 1, totalSum = 0, i = 1, result = 0;
+        for (; i < number; holder = i, i = totalSum) {
+            if (i + holder > number) {
                 break;
             }
             totalSum = i + holder;
-            if(totalSum% 2 ==0){
+            if (totalSum % 2 == 0) {
                 result += totalSum;
             }
         }
