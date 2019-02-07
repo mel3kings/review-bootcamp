d324b887e37d1a7f77d35c8d6489c9e4467345e2
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2019-02-08 01:38:21 +1100

    tweak twoSum

--- algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/TwoSum.java
+++ algorithm/Algorithm/src/main/java/crackingcode/stringsAndArrays/TwoSum.java
@@ -7,19 +7,16 @@ public class TwoSum {
 
     /**
      * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-     *
      * You may assume that each input would have exactly one solution, and you may not use the same element twice.
-     *
      * Example:
-     *
      * Given nums = [2, 7, 11, 15], target = 9,
-     *
      * Because nums[0] + nums[1] = 2 + 7 = 9,
      * return [0, 1].
+     *
      * @param args
      */
     public static void main(String[] args) {
-        Arrays.stream(twoSum(new int[]{3,3}, 6)).forEach(System.out::println);
+        Arrays.stream(twoSum(new int[]{2, 7, 11, 15}, 9)).forEach(System.out::println);
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -27,7 +24,6 @@ public class TwoSum {
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
         }
-
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement) && map.get(complement) != i) {
