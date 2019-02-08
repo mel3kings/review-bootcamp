b6da3fd36c570a6003a9e6ea0b4fd39d5d11970a
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2019-02-08 22:25:21 +1100

    divisible sum pair

---
+++ Hackerrank/src/easy/DivisibleSumPair.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class DivisibleSumPair {
+    public static void main(String[] args) {
+        System.out.println(divisibleSumPairs(6, 3, new int[]{1, 3, 2, 6, 1, 2}));
+
+    }
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int counter = 0;
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = i + 1; j < ar.length; j++) {
+                if ((ar[i] + ar[j]) % k == 0) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
