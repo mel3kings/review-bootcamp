edb56067a0aa045969aa49f924736fcb7e0ab2ee
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2019-02-10 19:38:48 +1100

    Dictionary step 1

--- algorithm/Algorithm/src/main/java/techguide/Dictionary.java
+++ algorithm/Algorithm/src/main/java/techguide/Dictionary.java
@@ -1,26 +1,53 @@
 package techguide;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.function.Function;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Dictionary {
 
     public static void main(String[] args) {
         Dictionary d = new Dictionary();
-        d.match("appbble", new String[]{"apple", "banana", "cat"});
+        d.match("appbananable", new String[]{"apple", "banana", "cat", "acid"});
+        List<String> wordsList = Arrays.asList("hello", "bye", "ciao", "bye", "ciao");
+        // a p p b a n a n a p b l e
+        // apple
+        // banana
+        // cat
+
+        //words in play map
+        // apple -> appple
+        // banana ->
+        // acid ->
 
     }
 
     public static boolean match(String word, String[] dictionary) {
-        Map<String, Long> wordMap = IntStream.range(0, word.length())
-                .mapToObj(n -> String.valueOf(word.charAt(n)))
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        HashMap<Character, List<String>> dictionaryMap =
+                Arrays.stream(dictionary).collect(groupingBy(val -> val.charAt(0),
+                        HashMap::new,
+                        Collectors.toList()));
+        HashMap<String, String> wordsInPlay = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i);
+            if (dictionaryMap.containsKey(c)) {
+                dictionaryMap.get(c).stream().forEach(play -> wordsInPlay.computeIfAbsent(play, v -> String.valueOf(c)));
+            } else {
+                for (String keys : wordsInPlay.keySet()) {
+                    if (keys.contains(String.valueOf(c))) {
+                        wordsInPlay.compute(keys, (k, v) -> v + c);
+                    }
+                }
 
-
-        System.out.println(wordMap);
+            }
+        }
         return false;
+    }
+
+    private static String[] hello(String a) {
+        return a.split("");
     }
 }
