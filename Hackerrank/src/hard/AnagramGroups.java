0649d9f8af4adbf2abc347449ae400d1b4fb7466
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2019-02-10 18:31:46 +1100

    optimize code

--- Hackerrank/src/hard/AnagramGroups.java
+++ Hackerrank/src/hard/AnagramGroups.java
@@ -10,37 +10,32 @@ import java.util.stream.Collectors;
 public class AnagramGroups {
 
     public static void main(String[] args) {
-        System.out.println(sameContents(new String[]{"abcd", "testering", "a"}, new String[]{"testering", "a", "abcd",
-                "EXE"}));
-        friendlyWords2(new String[]{"car", "cheating", "dale", "deal", "lead", "listen", "silent", "teaching",
+        System.out.println(sameContents(new String[]{"abcd", "testering", "a"}, new String[]{"testering", "a", "abcd"}));
+        friendlyWords(new String[]{"car", "cheating", "dale", "deal", "lead", "listen", "silent", "teaching",
                 "beaching", "dale"});
     }
 
-
     private static boolean sameContents(String[] a, String[] b) {
-        if (null == a || null == b) {
-            return false;
-        }
-        Map<String, Long> map = Arrays.stream(a)
-                .filter(val -> val != null && !val.equals(""))
-                .collect(Collectors.groupingBy(Function.identity(),
-                        java.util.stream.Collectors.counting()));
+        if (null == a || null == b) { return false; }
+        Map<String, Long> map = Arrays.stream(a).filter(val -> val != null && !val.equals(""))
+                .collect(Collectors.groupingBy(Function.identity(), java.util.stream.Collectors.counting()));
         Arrays.stream(b).forEach(bVal -> map.compute(bVal, (k, v) -> null == v ? -1 : v - 1));
-        return !map.values().stream().anyMatch(value -> value != 0);
+        return map.values().stream().noneMatch(value -> value != 0);
     }
 
-
-    static String[] friendlyWords2(String[] input) {
-        Map<Integer, List<String>> friendlyWordsMap =
-                Arrays.stream(input).collect(Collectors.groupingBy(x -> anagramHashCode(x)));
-        for (List<String> list : friendlyWordsMap.values()) {
+    /**
+     * Groups words by hashcode and groups them by sound alikes
+     * @param input
+     */
+    static void friendlyWords(String[] input) {
+        Map<Integer, List<String>> friendlyWordsMap = Arrays.stream(input)
+                .collect(Collectors.groupingBy(AnagramGroups::anagramHashCode));
+        friendlyWordsMap.values().stream().forEach(list -> {
             list.sort(Comparator.comparing(Object::toString));
             if (list.size() > 1) {
-                list.stream().forEach(x -> System.out.print(x + " "));
-                System.out.println("");
+                System.out.println(String.join(" ", list));
             }
-        }
-        return new String[]{};
+        });
     }
 
     private static int anagramHashCode(String input) {
