ccf06f22caee9a27aaa6ce849ade1ccdddc5b054
mel3kings <meltatlonghari3@gmail.com>
2020-03-14 15:24:29 +1100

    tree level

--- Hackerrank/src/easy/GetTreeLevel.java
+++ Hackerrank/src/easy/GetTreeLevel.java
@@ -1,4 +1,71 @@
 package easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * //
+ */
+
+class   TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
 public class GetTreeLevel {
+    public int maxDepth(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        queue.add(null);
+        int level = 0 ;
+
+        while(!queue.isEmpty()){
+            TreeNode currentNode = queue.pop();
+            if(currentNode.left != null){
+                queue.add(currentNode.left);
+            }
+            if (currentNode.right != null){
+                queue.add(currentNode.right);
+            }
+
+            if(null == queue.peek()){
+                level +=1;
+                queue.add(null);
+                queue.pop();
+                if(queue.peek() == null){
+                    break;
+                }
+            }
+        }
+        return level;
+    }
+
+
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(3);
+        TreeNode left = new TreeNode(9);
+        TreeNode right = new TreeNode(20);
+        root.left= left;
+        root.right= right;
+
+
+        TreeNode left2 = new TreeNode(9);
+        TreeNode right3 = new TreeNode(20);
+
+        left.left = left2;
+        left.right = right3;
+
+        GetTreeLevel level = new GetTreeLevel();
+        System.out.println(level.maxDepth(root));
+
+    }
+
+
 }
--- Hackerrank/src/easy/GetTreeLevel_Ans.java
+++ Hackerrank/src/easy/GetTreeLevel_Ans.java
@@ -1,4 +1,43 @@
 package easy;
 
+import java.util.LinkedList;
+
 public class GetTreeLevel_Ans {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        if(root.left == null && root.right == null) {
+            return 1;
+        }
+        int leftDepth = 0;
+        int rightDepth = 0;
+        if(root.left != null) {
+            leftDepth = maxDepth(root.left) + 1;
+        }
+        if(root.right != null) {
+            rightDepth = maxDepth(root.right) + 1;
+        }
+        return leftDepth > rightDepth ? leftDepth : rightDepth;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode left = new TreeNode(9);
+        TreeNode right = new TreeNode(20);
+        root.left = left;
+        root.right = right;
+
+
+        TreeNode left2 = new TreeNode(7);
+        TreeNode right3 = new TreeNode(2);
+
+        left.right = right3;
+        right.right =left2;
+        GetTreeLevel_Ans level = new GetTreeLevel_Ans();
+        System.out.println(level.maxDepth(root));
+
+    }
 }
+
+
