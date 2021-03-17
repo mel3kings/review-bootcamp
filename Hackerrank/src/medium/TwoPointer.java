5a03e87a776851c188a14e230e4cdae261700b8e
mel3kings <meltatlonghari3@gmail.com>
2021-03-17 21:08:47 +1100

    two pointers

---
+++ Hackerrank/src/medium/TwoPointer.java
@@ -0,0 +1,27 @@
+package medium;
+
+public class TwoPointer {
+        public int[] prisonAfterNDays(int[] cells, int n) {
+            int[] response = Arrays.copyOfRange(cells, 0, cells.length);
+            for(int i = 0; i < n; i++){
+
+                for(int j = 0; j < cells.length; j++){
+
+                    if(j == 0 || j == cells.length-1){
+                        response[j] = 0;
+                        continue;
+                    }
+                    int left = j-1, right = j+1;
+                    if(cells[left] == cells[right]){
+                        response[j] = 1;
+                    }else{
+                        response[j] = 0;
+                    }
+                    left++;
+                    right++;
+                }
+                cells = Arrays.copyOfRange(response, 0, response.length);
+            }
+            return response;
+    }
+}
--- Hackerrank/src/recording/DecodeWays/NumWays.java
+++ Hackerrank/src/recording/DecodeWays/NumWays.java
@@ -1,44 +1,57 @@
 package recording.DecodeWays;
 
-public class NumWays {
+import java.util.*;
 
-    // TODO REFRACTOR
+public class NumWays {
     int[] dp;
 
     public int numDecodings(String s) {
         dp = new int[s.length()];
-        return numWays(s);
-
+        Arrays.fill(dp, -1);
+        return numWays(s, s.length());
     }
 
-    public int numWays(String s) {
-        int currLength = s.length();
-        if (currLength == 1) {
+
+    public int numWays(String s, int length) {
+        if (dp[length] != -1) {
+            return dp[length];
+        }
+        if (length == 1) {
             int firstNumber = Integer.parseInt(s);
-            if (firstNumber == 0) {
-                return 0;
-            }
-            return 1;
-        } else if (currLength == 2) {
+            int response = firstNumber == 0 ? 0 : 1;
+            dp[length] = response;
+            return response;
+        } else if (length == 2) {
+            String firstVal = s.substring(0, 1);
+            int response;
             int firstTwo = Integer.parseInt(s);
-            if (firstTwo > 26 || firstTwo == 10 || firstTwo == 20) {
-                return 1;
+            if (firstVal.equals("0")) {
+                response = 0;
+            } else if (firstTwo > 26 || firstTwo == 10 || firstTwo == 20) {
+                response = 1;
             } else if (firstTwo == 0) {
-                return 0;
-            }
-            return 2;
-        } else {
-            String lastTwo = s.substring(currLength - 2, currLength);
-            int lastTwoInt = Integer.parseInt(lastTwo);
-            if (lastTwoInt == 0) {
-                return 0;
-            } else if (lastTwoInt > 26) {
-                int result = numWays(s.substring(0, currLength - 1));
-                return result;
+                response = 0;
             } else {
-                int result = numWays(s.substring(0, currLength - 1)) + numWays(s.substring(0, currLength - 2));
-                return result;
+                response = 2;
             }
+            dp[length] = response;
+            return response;
+        } else {
+            String lastTwo = s.substring(length - 2, length);
+            int lastTwoInt = Integer.parseInt(lastTwo);
+            int response;
+            if (lastTwoInt == 0) {
+                response = 0;
+            } else if (lastTwoInt > 26) {
+                String sub = s.substring(0, length - 1);
+                response = numWays(sub, sub.length());
+            } else {
+                String sub = s.substring(0, length - 1);
+                String sub2 = s.substring(0, length - 2);
+                response = numWays(sub, sub.length()) + numWays(sub2, sub2.length());
+            }
+            dp[length] = response;
+            return response;
         }
     }
 }
